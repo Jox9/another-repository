5a77afbd4fb60fe7db1900804c45d984a3b874ec
Lyn <correo@email.com>
2024-01-20 20:24:42 -0600

    Ajustes de texto a main.py
    Se corrige el contenido de los textos, para que sean coherentes.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+favorite_plant = str()
+
 def saludar() -> None:
     name = input("Escriba su nombre: ")
     msj = f"Hola {name}, tenga un buen dia.  "
@@ -6,14 +8,15 @@ def saludar() -> None:
 def ask_plant() -> None:
     msj = "Me gustan las plantas."
     print(msj)
-    favorite = input("Cual es tu planta favorito ? ")
-    msj = f"El {favorite} !!. Es un bonita planta"
+    favorite = input("Cual es tu planta favorita ? ")
+    favorite_plant = favorite
+    msj = f"El {favorite} !!. Es una bonita planta"
     print(msj)
 
 def ask_plant_name()-> None:
     name = input("Cual es su nombre? ")
-    msj = f"""{name}?, que lindo nombre.
-    Suena a que nos vamos a llevar bien.""" 
+    msj = f"""{name}?, que lindo nombre, para un {favorite_plant}
+    Suena a que tu planta y yo, nos vamos a llevar bien.""" 
     print(msj)
     
 saludar()
