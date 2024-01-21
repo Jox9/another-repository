ef6c2fa7a866f0c3519d92900e40528908cae338
Lyn <correo@email.com>
2024-01-20 20:33:39 -0600

    Elimina la linea de la planta favorita main.py
    Ya que no se logró corregir correctamente el error.
    Se elimino la funcionalidad del programa que mencionaba la planta favorita.

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-favorite_plant = str()
-
 def saludar() -> None:
     name = input("Escriba su nombre: ")
     msj = f"Hola {name}, tenga un buen dia.  "
     print(msj)
 
-def ask_plant(favorite_plant) -> None:
+def ask_plant() -> None:
     msj = "Me gustan las plantas."
     print(msj)
     favorite = input("Cual es tu planta favorita ? ")
-    favorite_plant = favorite
     msj = f"El {favorite} !!. Es una bonita planta"
     print(msj)
 
-def ask_plant_name()-> None:
+def ask_plant_name(favorite_plant)-> None:
     name = input("Cual es su nombre? ")
-    msj = f"""{name}?, que lindo nombre, para un {favorite_plant}
+    msj = f"""{name}?, que lindo nombre.
 Suena a que tu planta y yo, nos vamos a llevar bien.""" 
     print(msj)
     
 saludar()
-ask_plant(favorite_plant)
+ask_plant()
 ask_plant_name()
