eaa507ff25b5d31ac7f3d43e5b909fc6b76ba788
Lyn <correo@email.com>
2024-01-20 18:11:44 -0600

    Nueva funcion para main.py, ask_animal_name()
    Actualiza el archivo main.py, el cual añade una nueva funcion para,
    interactuar con el nombre del animal del usuario, una vez es preguntado.

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ def ask_animal() -> None:
     favorite = input("Cual es tu animal favorito ? ")
     msj = f"El {favorite} !!. Es un bonito animal"
     print(msj)
+
+def ask_animal_name()-> None:
+    name = input("Cual es su nombre? ")
+    msj = f"""{name}?, que lindo nombre.
+    Suena a que nos vamos a llevar bien.""" 
+    print(msj)
     
 saludar()
 ask_animal()
+ask_animal_name()
