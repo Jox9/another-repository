e95f74dead2b8532ed24ae8f1733c3e7077a934e
Lyn <correo@email.com>
2024-01-20 19:24:08 -0600

    main.py pero orientado a plantas
    modifica la funciones para hablar acerca de plantas en vez de animales.

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ def saludar() -> None:
     msj = f"Hola {name}, tenga un buen dia.  "
     print(msj)
 
-def ask_animal() -> None:
-    msj = "Me gustan los animales."
+def ask_plant() -> None:
+    msj = "Me gustan las plantas."
     print(msj)
-    favorite = input("Cual es tu animal favorito ? ")
-    msj = f"El {favorite} !!. Es un bonito animal"
+    favorite = input("Cual es tu planta favorito ? ")
+    msj = f"El {favorite} !!. Es un bonita planta"
     print(msj)
 
-def ask_animal_name()-> None:
+def ask_plant_name()-> None:
     name = input("Cual es su nombre? ")
     msj = f"""{name}?, que lindo nombre.
     Suena a que nos vamos a llevar bien.""" 
     print(msj)
     
 saludar()
-ask_animal()
-ask_animal_name()
+ask_plant()
+ask_plant_name()
